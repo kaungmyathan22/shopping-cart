fe35c0a3d4bab1bf9e79c379d1dbeb1083a5e8ef
Kaung Myat Han <arkarhtethan@gmail.com>
2019-07-19 19:31:15 +0630

    Added model to admin

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
-
+from .models import Product, Category
 # Register your models here.
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'created', 'updated', 'slug', )
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'category', 'price',
+                    'created', 'updated', 'slug', )
+
+    list_editable = ('category', 'price',)
+
+    search_fields = ('title', 'category', 'price', 'slug', )
+
+    list_filter = ['category', 'created']
