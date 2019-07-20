0b0d3cd74f74729b9de18a182106f145e16c3e1d
Kaung Myat Han <arkarhtethan@gmail.com>
2019-07-20 15:22:34 +0630

    product-detail url added

---
+++ product/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import product_list, product_detail
+
+app_name = "product"
+
+urlpatterns = [
+    path('', product_list, name="list"),
+    path('detail/<slug:slug>/', product_detail, name="detail"),
+]
