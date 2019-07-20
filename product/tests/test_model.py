b01a4265f518d2851da1e78ab7bc8de315e0a721
Kaung Myat Han <arkarhtethan@gmail.com>
2019-07-20 16:14:52 +0630

    model test case added

--- product/tests/test_model.py
+++ product/tests/test_model.py
@@ -1,7 +1,8 @@
 from django.test import TestCase, Client
 from django.urls import reverse, resolve
 from model_mommy import mommy
-from product.models import Product, Category
+from product.models import Product, Category, image_upload_path
+from utils.unique_slug_field_generator import unique_slug_generator
 
 
 class TestProductModel(TestCase):
@@ -25,6 +26,41 @@ class TestProductModel(TestCase):
     def test_slug(self):
 
         self.assertTrue(len(self.model_instance.slug) > 0)
+
+    def test_upload_field_with_model_instance(self):
+
+        file_name = "hello.png"
+
+        uploaded_file_name = image_upload_path(self.model_instance, file_name)
+
+        expected = f'hello-{self.model_instance.slug}.png'
+
+        self.assertEqual(uploaded_file_name, expected)
+
+    def test_upload_field_without_model_instance(self):
+
+        category = mommy.make("Category")
+
+        model = Product(category, 'title', 12.99, 'test')
+
+        file_name = "hello.png"
+
+        uploaded_file_name = image_upload_path(model, file_name)
+
+        self.assertNotEqual(uploaded_file_name, file_name)
+
+    def test_slug_unique(self):
+
+        category = mommy.make("Category")
+
+        model = mommy.make("Product")
+        # model = Product(category, 'title', 12.99, 'test')
+
+        slug = unique_slug_generator(model)
+
+        slug_2 = unique_slug_generator(model)
+
+        self.assertNotEqual(slug, slug_2)
 
 
 class TestCategorytModel(TestCase):
