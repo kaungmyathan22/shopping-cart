484bf3403d11b49168a8bbce433a42d7654eb419
Kaung Myat Han <arkarhtethan@gmail.com>
2019-07-22 11:19:55 +0630

    order create view added

--- order/views.py
+++ order/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from .forms import OrderCreateForm
 # Create your views here.
+
+
+def order_create(request):
+
+    form = OrderCreateForm(request.POST or None)
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'order/order_create.html', context)
