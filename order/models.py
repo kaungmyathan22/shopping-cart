9e77fb3a34c8ed40d0bfb1b1f95b8a069941e3f4
Kaung Myat Han <arkarhtethan@gmail.com>
2019-07-22 11:20:16 +0630

    Order, OrderItem model created

--- order/models.py
+++ order/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from product.models import Product
 
-# Create your models here.
+
+class Order(models.Model):
+
+    first_name = models.CharField(max_length=120)
+
+    last_name = models.CharField(max_length=120)
+
+    email = models.EmailField()
+
+    phone = models.CharField(max_length=10)
+
+    city = models.CharField(max_length=120)
+
+    address = models.CharField(max_length=120)
+
+    paid = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def get_total_cose(self):
+
+        return sum(item.get_cost() for item in self.items)
+
+    def __str__(self):
+
+        return f'Order {self.id}'
+
+
+class OrderItem(models.Model):
+
+    order = models.ForeignKey(
+        to="Order", related_name="items", on_delete=models.CASCADE)
+
+    product = models.ForeignKey(to=Product, on_delete=models.DO_NOTHING)
+
+    quantity = models.PositiveIntegerField()
+
+    price = models.DecimalField(max_digits=15, decimal_places=2)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def get_cost(self):
+
+        return self.price * self.quantity
+
+    def __str__(self):
+
+        return str(self.pk)
